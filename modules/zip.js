af116768c22eaac60b10e90fbe00f3581d4c3600
Logan Esenther <logan@cal.berkeley.edu>
2019-09-09 16:03:23 -0700

    Added new primary cracking method for zip files

--- modules/zip.js
+++ modules/zip.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
+
+const { basename } = require('path');
+
+const node7z = require('node-7z');
 
 /**
  * Try to unzip a file using a specified password
@@ -10,6 +14,27 @@ const { exec } = require('child_process');
  * @returns {Promise} Resolves with the password if password is correct, false if password is wrong, or rejects on error.
  */
 function tryPassword(path, password) {
+    return new Promise((resolve, reject) => {
+        const extraction = node7z.extractFull(path, path.replace(basename(path), ''), { password });
+        extraction.on('end', _ => resolve(password));
+        extraction.on('error', error => {
+            if (error.message.indexOf('Wrong password') === -1) {
+                return reject(error);
+            }
+            
+            return resolve(false);
+        });
+    });
+}
+
+/**
+ * Try to unzip a file using a specified password
+ *
+ * @param {String} path Path of the zip file
+ * @param {String} password Password to try to unzip the file with
+ * @returns {Promise} Resolves with the password if password is correct, false if password is wrong, or rejects on error.
+ */
+function tryPasswordSecondary(path, password) {
     return new Promise((resolve, reject) => {
         exec(`unzip -o -P "${password}" ${path}`, (err, stdout, stderr) => {
             if (err) {
@@ -25,4 +50,14 @@ function tryPassword(path, password) {
     });
 }
 
-module.exports = tryPassword;
+function chooseMethod() {
+    try {
+        execSync(`7z`, { stdio: 'ignore' });
+
+        return tryPassword;
+    } catch(error) {        
+        return tryPasswordSecondary;
+    }
+}
+
+module.exports = chooseMethod();
